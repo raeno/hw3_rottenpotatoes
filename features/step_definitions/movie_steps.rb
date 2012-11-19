3468f9476d3e32b95b32c938fb725783a9042fc9
raeno <just.raeno@gmail.com>
2012-11-19 23:44:09 +0400

    Добавлено заполнение базы необходимыми фильмами. Проходят тесты filter_movie и search_movie

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create(:title => movie[:title].strip, :rating => movie[:rating].strip, :release_date => movie[:release_date].strip)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -16,6 +14,20 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  page.body is the entire content of the page as a string.
   flunk "Unimplemented"
 end
+
+Given /I have added "(.*)" with rating "(.*)"/ do |title, rating|
+  Given %Q{I am on the Create New Movie page}
+  When %Q{I fill in "Title" with "#{title}"}
+  And %Q{I select "#{rating}" from "Rating"}
+  And %Q{I press "Save Changes"}
+end
+
+Then /I should see "(.*)" before "(.*)" on "(.*)"/ do |string1,string2,path|
+  Given %Q{I am on #{path}}
+  regexp = Regexp.new ".*#{string1}.*#{string2}"
+  page.body.should =~ regexp
+end
+
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
